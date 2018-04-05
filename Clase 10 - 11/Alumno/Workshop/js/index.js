64319d610b413bb05c7d86c99484235073159ac3
ines <ines@mobomo.com>
2018-04-04 23:52:11 -0300

    Workshop clases 10/11 - parte 2 (incompleta)

--- Clase 10 - 11/Alumno/Workshop/js/index.js
+++ Clase 10 - 11/Alumno/Workshop/js/index.js
@@ -25,16 +25,64 @@ firstName.onblur = function (event) {
     allowAddButton()
 }
 
-// PARSEAR NÚMERO:
 
-function parseNumber(number) {
-    var parsedNumber = parseInt(number, 10)
-    if (isNaN(parsedNumber)) {
-        return false
-    } else {
-        return parsedNumber
+// VALIDAR APELLIDO:
+
+var lastName = document.getElementById('lastName')
+
+lastName.onblur = function (event) {
+
+    var lastNameNode = event.target
+    var value = lastNameNode.value
+    var parentlastNameNode = lastNameNode.parentElement
+    var lastNameErrorNode = document.getElementById('lastNameError')
+
+    if (lastNameErrorNode) {
+        parentlastNameNode.removeChild(lastNameErrorNode)
     }
+
+    if (value && typeof value === 'string') {
+        showValidInput(lastNameNode)
+    } else {
+        showInvalidInput(lastNameNode)
+        lastNameErrorNode = document.createElement('span')
+        lastNameErrorNode.id = 'lastNameError'
+        lastNameErrorNode.innerHTML = 'Completar el apellido'
+        parentlastNameNode.appendChild(lastNameErrorNode)
+    }
+    allowAddButton()
 }
+
+
+
+// VALIDAR E-MAIL:
+
+var email = document.getElementById('email')
+
+email.onblur = function (event) {
+
+    var emailNode = event.target
+    var value = emailNode.value
+    var parentemailNode = emailNode.parentElement
+    var emailErrorNode = document.getElementById('emailError')
+
+    if (emailErrorNode) {
+        parentemailNode.removeChild(emailErrorNode)
+    }
+
+    if (value && typeof value === 'string' && value.indexOf('@') > -1 && value.indexOf('.') > -1) {
+        showValidInput(emailNode)
+    } else {
+        showInvalidInput(emailNode)
+        emailErrorNode = document.createElement('span')
+        emailErrorNode.id = 'emailError'
+        emailErrorNode.innerHTML = 'Ingrese un e-mail valido.'
+        parentemailNode.appendChild(emailErrorNode)
+    }
+    allowAddButton()
+}
+
+
 
 // VALIDAR DNI (AGREGAR):
 
@@ -104,12 +152,12 @@ deleteDni.onblur = function (event) {
 }
 
 
+
 // HABILITAR EL BOTÓN DE AGREGAR ALUMNO
 
 function allowAddButton () {
     var addStudentButton = document.getElementById('addStudentButton')
-    var validFields = document.getElementsByClassName('is-valid')
-    if (firstName.classList.contains('is-valid') && dni.classList.contains('is-valid')) { // Si los 2 campos son válidos
+    if (firstName.classList.contains('is-valid') && lastName.classList.contains('is-valid') && dni.classList.contains('is-valid') && email.classList.contains('is-valid')) { // Si los 4 campos son válidos
         addStudentButton.disabled = false
     } else {
         addStudentButton.disabled = true
@@ -130,11 +178,24 @@ function allowDeleteButton () {
 }
 
 
+// HABILITAR EL BOTÓN DE BUSCAR
+
+function allowSearchButton () {
+    var searchStudentButton = document.getElementById('searchStudentButton')
+    var searchText = document.getElementById('searchText')
+    if (searchText.classList.contains('is-valid')) { // Si el input de buscar es válido
+        searchStudent.disabled = false
+    } else {
+        searchStudent.disabled = true
+    }
+}
+
+
 // AGREGAR ALUMNO AL ARRAY + LOCAL STORAGE
 
 var studentList = []
-function agregarAlumno (firstName, dni) {
-    studentList.push({"firstName": firstName, "dni": dni})
+function agregarAlumno (firstName, lastName, dni, email) {
+    studentList.push({"firstName": firstName, "lastName": lastName, "dni": dni, "email": email})
     var stringStudentList = JSON.stringify(studentList)
     localStorage.setItem('students', stringStudentList)
     console.log(studentList)
@@ -143,23 +204,26 @@ function agregarAlumno (firstName, dni) {
 
 // AGREGAR ALUMNO A LA LISTA HTML
 
-function addStudentLi (firstName, dni) {
-
+function addStudentLi (firstName, lastName, dni, email) {
     var mainList = document.getElementById('mainList')
-    dni = document.getElementById('dni')
-    firstName = document.getElementById('firstName')
+    var dni = document.getElementById('dni')
+    var firstName = document.getElementById('firstName')
+    var lastName = document.getElementById('lastName')
+    var email = document.getElementById('email')
     var dniValue = dni.value
     var firstNameValue = firstName.value
+    var lastNameValue = lastName.value
+    var emailValue = email.value
 
     var li = document.createElement('li')
     li.className = 'list-group-item '
     li.id = dniValue
     var fullDni = 'DNI: ' + dniValue
 
-    li.innerHTML = '<h1>' + firstNameValue + '</h1><p>' + fullDni
+    li.innerHTML = '<h1>' + firstNameValue + '</h1><h2>' + lastNameValue +  '</h2><p>' + fullDni + '<br>' + emailValue + '</p>'
 
     mainList.appendChild(li)
-    agregarAlumno(firstNameValue, dniValue)
+    agregarAlumno(firstNameValue, lastNameValue, dniValue, emailValue)
     resetAllInputs()
 }
 
@@ -177,6 +241,7 @@ function borrarAlumno (value) {
         var value = deleteDni.value
 
         if (dni.indexOf(value) > -1 && alumno.dni === value) {
+
             // Borrar del array y updatear local storage:
             studentList.splice(i, 1)
             var stringStudentList = JSON.stringify(studentList)
@@ -190,6 +255,55 @@ function borrarAlumno (value) {
         }
     }
     resetAllInputs()
+}
+
+
+// HACER BÚSQUEDA
+
+function buscarNombre () {
+
+    var searchList = document.getElementById('searchList')
+    var searchText = document.getElementById('searchText')
+    var searchTextValue = searchText.value
+
+    var index = buscar(searchTextValue)
+
+    if (index !== -1) {
+
+        // Cómo sería la parte de copiar el <li> correspondiente y pegarlo en la lista de Búsqueda?
+
+    } else {
+        console.log('Nombre no encontrado.')
+    }
+}
+
+
+// PASAR A MAYÚSCULAS y BUSCAR
+
+function pasarAMayusculas (studentName) {
+    if (typeof studentName === 'string') {
+        return studentName.toUpperCase()
+    }
+}
+
+function buscar (studentName) {
+    var index = -1
+    var studentUpperCase = pasarAMayusculas(studentName)
+    console.log('Término ingresado: ' + studentName)
+
+    for (var i = 0; i < studentList.length; i++) {
+        var alumno = studentList[i]
+        var firstName = alumno.firstName
+        var firstNameUppercase = pasarAMayusculas(firstName)
+        var lastName = alumno.lastName
+        var lastNameUppercase = pasarAMayusculas(lastName)
+        if (firstNameUppercase.indexOf(studentUpperCase) !== -1  || lastNameUppercase.indexOf(studentUpperCase) !== -1) {
+            index = i
+            console.log('Nombre encontrado: ' + firstName + ' ' + lastName + ' / index: ' + index)
+            break
+        }
+    }
+    return index
 }
 
 
@@ -207,14 +321,27 @@ var deleteStudentButton = document.getElementById('deleteStudentButton')
 deleteStudentButton.onclick = borrarAlumno
 
 
+// BUSCAR AL CLICK
+
+var searchStudentButton = document.getElementById('searchStudentButton')
+searchStudentButton.onclick = buscarNombre
+
+
+// RESETEAR TODOS LOS CAMPOS
 
 function resetAllInputs () {
     firstName.value = ''
     firstName.classList.remove('is-valid')
     firstName.classList.remove('is-invalid')
+    lastName.value = ''
+    lastName.classList.remove('is-valid')
+    lastName.classList.remove('is-invalid')
     dni.value = ''
     dni.classList.remove('is-valid')
     dni.classList.remove('is-invalid')
+    email.value = ''
+    email.classList.remove('is-valid')
+    email.classList.remove('is-invalid')
     addStudentButton.disabled = true
     deleteDni.value = ''
     deleteDni.classList.remove('is-valid')
@@ -233,5 +360,17 @@ function showInvalidInput (inputNode) {
     if (inputNode && inputNode.classList) {
         inputNode.classList.remove('is-valid')
         inputNode.classList.add('is-invalid')
+    }
+}
+
+
+// PARSEAR NÚMERO:
+
+function parseNumber(number) {
+    var parsedNumber = parseInt(number, 10)
+    if (isNaN(parsedNumber)) {
+        return false
+    } else {
+        return parsedNumber
     }
 }
